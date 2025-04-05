0047967e5199b708f52fe85d3fb603d98964e2a5
BichriTech <root@MacBookPro.lan>
2025-04-05 00:33:13 -0400

    heroku config

--- src/main/java/com/bichri/Client_Mgr/ClientMgrApplication.java
+++ src/main/java/com/bichri/Client_Mgr/ClientMgrApplication.java
@@ -2,8 +2,10 @@ package com.bichri.Client_Mgr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.yml")
 public class ClientMgrApplication {
 
 	public static void main(String[] args) {
